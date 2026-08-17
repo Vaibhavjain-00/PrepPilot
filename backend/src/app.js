417360d085cd5ea00727976a91df799ca0fe4d39
Vaibhav Jain <vaibhavjain0220@gmail.com>
2026-08-18 02:49:14 +0530

    fix cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://prep-pilot-eosin-pi.vercel.app",
     credentials: true,
   }),
 );
